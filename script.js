dde13cfca5aaf98d9c37f2c511ecd94315f53d69
Chris <ckatanic@gmail.com>
2023-11-08 19:15:01 -0800

    work on modal search result controls

--- script.js
+++ script.js
@@ -4,24 +4,12 @@ const searchInput = document.getElementById('searchInput');
 const addBookButton = document.getElementById("addBookButton");
 const addBookModal = document.getElementById("addBookModal");
 const closeModal = document.getElementById("closeModal");
-const modalContent = document.getElementById('modal-content');
+const searchResults = document.getElementById('search-results');
+const addToLibraryButton = document.getElementById('addToLibraryButton');
+const booksDisplay = document.getElementById('books-display');
+const controls = document.getElementsByClassName('controls')[0];
 
-searchForm.addEventListener("submit", async function(e) {
-    e.preventDefault();
-    bookData = await getBookData(searchInput.value);
-    searchInput.value="";
-    console.log(bookData);
-    const bookCard = createBookCard(bookData);
-    modalContent.appendChild(bookCard);
-})
-
-addBookButton.addEventListener("click", () => {
-    addBookModal.style.display="grid";
-})
-
-closeModal.addEventListener("click", () => {
-    addBookModal.style.display="none";
-})
+const apiKey = "AIzaSyDigPDuzzDm-W4SQl-9xv5i2IBEBmVeOLs";
 
 function Book(title, author, numPages, haveRead) {
     this.title = title;
@@ -34,41 +22,88 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.haveRead}`;
 }
 
-const booksInLibrary = [];
-const apiKey = "AIzaSyDigPDuzzDm-W4SQl-9xv5i2IBEBmVeOLs";
+const library = {
+    booksInLibrary: [],
+    allSearchResults: {},
+    currentBookToAddCandidate: {},
 
-async function getBookData(book) {
-    const requestedData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${apiKey}`)
-    const bookData = await requestedData.json();
-    return bookData;
+    getBookData: async function(book) {
+        const requestedData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${apiKey}`)
+        const bookData = await requestedData.json();
+        library.allSearchResults = bookData;
+        return bookData;
+    },
+    createBookCard: function(bookData) {
+        const bookCard = library.createElement('div', "");
+        bookCard.setAttribute('class', "card");
+
+        const imageDiv = library.createElement('div', "");
+        imageDiv.setAttribute('class', 'image');
+
+        const image = library.createElement('img', "");
+        image.src=`${bookData.items[0].volumeInfo.imageLinks.thumbnail}`
+        imageDiv.append(image);
+        bookCard.append(imageDiv);
+
+        const bookInfo = library.createElement('div', "");
+        bookInfo.setAttribute('class', 'book-info');
+
+        const bookTitle = library.createElement('h2',`${bookData.items[0].volumeInfo.title}`);
+        bookInfo.appendChild(bookTitle);
+
+        const bookAuthors = library.createElement('p', `${bookData.items[0].volumeInfo.authors[0]}`);
+        bookInfo.appendChild(bookAuthors);
+
+        const numberOfPages = library.createElement('p', `${bookData.items[0].volumeInfo.pageCount}`);
+        bookInfo.appendChild(numberOfPages);
+        bookCard.appendChild(bookInfo);
+        library.currentBookToAddCandidate = bookCard;
+
+        return bookCard;
+    },
+    createElement: function(type, content) {
+        const el = document.createElement(type);
+        el.innerText = content;
+        return el;
+    },
+    removeSearchCandidate: function() {
+        cardToRemove = searchResults.children[0];
+        if (cardToRemove) {
+            cardToRemove.remove();
+        }
+    },
+    closeModal: function() {
+        addBookModal.style.display="none";
+        library.removeSearchCandidate();
+        document.getElementById('addToLibraryButton').style.display="none";
+        controls.style.display="none";
+    }
 }
 
-function createBookCard(bookData) {
-    const bookCard = createElement('div', "");
-    bookCard.setAttribute('class', "card");
-    const imageDiv = createElement('div', "");
-    imageDiv.setAttribute('class', 'image');
-    const image = createElement('img', "");
-    // console.log(bookData);
-    image.src=`${bookData.items[0].volumeInfo.imageLinks.thumbnail}`
-    imageDiv.append(image);
-    bookCard.append(imageDiv);
-    const bookInfo = createElement('div', "");
-    bookInfo.setAttribute('class', 'book-info');
-    const bookTitle = createElement('h2',`${bookData.items[0].volumeInfo.title}`);
-    bookInfo.appendChild(bookTitle);
-    const bookAuthors = createElement('p', `${bookData.items[0].volumeInfo.authors[0]}`);
-    bookInfo.appendChild(bookAuthors);
-    const numberOfPages = createElement('p', `${bookData.items[0].volumeInfo.pageCount}`);
-    bookInfo.appendChild(numberOfPages);
-    bookCard.appendChild(bookInfo);
-    return bookCard;
-}
+searchForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    if (searchResults.children.length > 0) {
+        library.removeSearchCandidate();
+    }
+    bookData = await library.getBookData(searchInput.value);
+    searchInput.value="";
+    console.log(bookData);
+    const bookCard = library.createBookCard(bookData);
+    searchResults.appendChild(bookCard);
+    addToLibraryButton.style.display="block";
+    controls.style.display="flex";
+})
 
-function createElement(type, content) {
-    const el = document.createElement(type);
-    el.innerText = content;
-    return el;
-}
+addBookButton.addEventListener("click", () => {
+    addBookModal.style.display="grid";
+})
+
+addToLibraryButton.addEventListener('click', () => {
+    library.booksInLibrary.push(library.allSearchResults.items[0]);
+    booksDisplay.appendChild(library.currentBookToAddCandidate);
+    library.closeModal();
+})
+
+closeModal.addEventListener("click", library.closeModal);
 
 
